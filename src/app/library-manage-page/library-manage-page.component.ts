ff76fafcc55daec559c39d1ae06c85a37d269d8f
Thomas Jan van Leeuwen <t.van.leeuwen@blueriq.com>
2025-03-05 12:25:21 +0100

    library manage page has a back button library page is middle aligned update dependencies

--- src/app/library-manage-page/library-manage-page.component.ts
+++ src/app/library-manage-page/library-manage-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { FaIconComponent, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { LibraryImportFolderComponent } from '../library-import-folder/library-import-folder.component';
 import { LibraryPathConfiguratorComponent } from '../library-path-configurator/library-path-configurator.component';
 import { LibraryImportResultsComponent } from '../library-import-results/library-import-results.component';
@@ -6,6 +9,7 @@ import { LibraryImportResultsComponent } from '../library-import-results/library
 @Component({
   selector: 'app-library-manage-page',
   imports: [
+    FaIconComponent,
     LibraryPathConfiguratorComponent,
     LibraryImportFolderComponent,
     LibraryImportResultsComponent,
@@ -13,4 +17,15 @@ import { LibraryImportResultsComponent } from '../library-import-results/library
   templateUrl: './library-manage-page.component.html',
   styleUrl: './library-manage-page.component.css',
 })
-export class LibraryManagePageComponent {}
+export class LibraryManagePageComponent {
+  private readonly router = inject(Router);
+  private readonly faIconLibrary = inject(FaIconLibrary);
+
+  constructor() {
+    this.faIconLibrary.addIcons(faArrowLeft);
+  }
+
+  async backToPlayback() {
+    await this.router.navigate(['playback-page']);
+  }
+}
